18ace560542920e868a4d0020be56752561dbe99
Max Zinkus <5474655+maxzinkus@users.noreply.github.com>
2025-05-03 14:01:49 -0700

    Optionally use a logger in bluescal

--- bluescal.py
+++ bluescal.py
@@ -6,17 +6,33 @@ import time
 CAL_URL = "https://calendar.google.com/calendar/ical/seattlebluesdancecollective%40gmail.com/public/basic.ics"
 CAL_FILE = "bluescal.ics"
 
-def refresh():
-    if os.path.exists(CAL_FILE):
-        if os.path.getmtime(CAL_FILE) > time.time() - 10:
-            with open(CAL_FILE, "r") as f:
-                return ical.Calendar.from_ical(f.read())
-    response = requests.get(CAL_URL)
-    if response.status_code == 200 and len(response.text) > 0 and len(response.text) < 1_048_576: # 1MiB
-        with open(CAL_FILE, "w") as f:
-            f.write(response.text)
-        calendar = ical.Calendar.from_ical(response.text)
-        return calendar
+def refresh(logger=None):
+    try:
+        if os.path.exists(CAL_FILE):
+            if logger:
+                logger.info("Calendar file exists")
+            if os.path.getmtime(CAL_FILE) > time.time() - 10:
+                if logger:
+                    logger.info("Using cached calendar file")
+                with open(CAL_FILE, "r") as f:
+                    return ical.Calendar.from_ical(f.read())
+            else:
+                if logger:
+                    logger.info("Calendar file is outdated; fetching new copy")
+        if logger:
+            logger.info("Fetching calendar from %s", CAL_URL)
+        response = requests.get(CAL_URL)
+        if response.status_code == 200 and len(response.text) > 0 and len(response.text) < 1_048_576: # 1MiB
+            if logger:
+                logger.info("Successfully fetched calendar (%d bytes)", len(response.text))
+            with open(CAL_FILE, "w") as f:
+                f.write(response.text)
+            calendar = ical.Calendar.from_ical(response.text)
+            return calendar
+    except Exception as e:
+        logger.error("Error fetching calendar: %s", e)
+    if logger:
+        logger.error("Failed to fetch calendar from %s", CAL_URL)
     return None
 
 if __name__ == "__main__":
