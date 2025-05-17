c543fb8efe9641f6aee5f1f30ee92732ca090fce
Max Zinkus <5474655+maxzinkus@users.noreply.github.com>
2025-05-17 16:55:44 -0700

    Improve date handling in main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import logging
 
 from flask import Flask, render_template, flash
-from datetime import datetime
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 import bluescal
 
@@ -18,31 +19,49 @@ def index():
         app.logger.error("No events found; a sync error may have occurred.")
         return render_template('index.html', events=[])
     events = []
-    for i, cal_event in enumerate(calendar.events):
+    for i, cal_event in enumerate(sorted(filter(lambda x: x.get("DTSTART"), calendar.events), key=lambda x: x["DTSTART"].dt)):
         event = {}
         event["id"] = str(i)
+        event["uid"] = str(cal_event.get("UID", str(hash(str(cal_event)))))
         event["title"] = cal_event.get("SUMMARY", "")
         
         # Parse the iCalendar date strings into datetime objects
         dtstart = cal_event.get("DTSTART")
         dtend = cal_event.get("DTEND")
-        if not dtstart or not dtend:
-            continue
 
         # TODO: get neighborhood from location: Maps? LLM? -> cache; https://stackoverflow.com/questions/13488759/is-it-possible-to-get-the-neighborhood-of-an-address-using-google-maps-api-or-bi
-        # TODO: get features for categories (Live Music, No Cover, Beginner Lesson): LLM -> cache
-        # TODO: convert from UTC and don't use 24-hour time: easy
 
-        event["date"] = dtstart.dt.date().strftime("%Y-%m-%d")
-        event["time"] = dtstart.dt.strftime("%H:%M") + " - " + dtend.dt.strftime("%H:%M")
+        utc_start = dtstart.dt.replace(tzinfo=timezone.utc)
+        local_start = utc_start.astimezone(ZoneInfo("America/Los_Angeles"))
+
+        if dtend:
+            utc_end = dtend.dt.replace(tzinfo=timezone.utc)
+            local_end = utc_end.astimezone(ZoneInfo("America/Los_Angeles"))
+        else:
+            local_end = local_start
+
+        event["date"] = local_start.date().strftime("%Y-%m-%d")
+        if local_start != local_end:
+            event["time"] = local_start.strftime("%-I:%M %p") + " - " + local_end.strftime("%-I:%M %p")
+        else:
+            event["time"] = local_start.strftime("%-I:%M %p")
         event["location"] = cal_event.get("LOCATION", "")
-        event["features"] = cal_event.get("CATEGORIES", [])
+        features = set()
+        if "live music" in cal_event.get("DESCRIPTION", "").lower():
+            features.add("Live Music")
+        if "lesson" in cal_event.get("DESCRIPTION", "").lower():
+            features.add("Lesson")
+        event["features"] = list(features)
         event["description"] = cal_event.get("DESCRIPTION", "")
         events.append(event)
     if not events:
         flash("No events found; a sync error may have occurred.")
         app.logger.error("No events found; a sync error may have occurred.")
     return render_template('index.html', events=events)
+
+@app.route('/recurring-events')
+def recurring_events():
+    return render_template('recurring_events.html')
 
 @app.route('/about')
 def about():
@@ -71,11 +90,6 @@ def contact():
 @app.route('/donate')
 def donate():
     return render_template('donate.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
